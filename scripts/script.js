322818016ca63a5ee005fb1032490c0a31c397af
AbrahamAz <abraham.azar30@outlook.com>
2021-11-09 10:55:31 +0200

    Adding animation to the mouse scroll

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,24 @@ $(document).ready(function () {
  * Down blocks, and every here and there it reachs a step were
  * it tell a section of my website
  */
+
+
+// moving the scrolling button of the mouse
+
+$(document).ready(function(){
+  animateDiv('.mouseScroll');
+});
+
+function makeNewPosition(){
+  var h = $(window).height() - 100;
+  var nh = Math.floor(h/30);
+  return [nh];
+};
+
+function animateDiv(myClass) {
+  var newq = makeNewPosition();
+  $(myClass).animate({top: newq[0]}, 1500, 'linear', function(){
+    $(myClass).css('top', '10px');
+    animateDiv(myClass);
+  });
+};
